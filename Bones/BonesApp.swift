eec9173cf72002ac308ffef840c92dba4312065b
fduarte896 <66535395+fduarte896@users.noreply.github.com>
2025-07-10 16:02:26 -0500

    Initial Commit

---
+++ Bones/BonesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BonesApp.swift
+//  Bones
+//
+//  Created by Felipe Duarte on 10/07/25.
+//
+
+import SwiftUI
+
+@main
+struct BonesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
